62fe64a5eb207e100a91af5dbda5c7a0bf861b51
faarbaek <lasse@faarbaek.dk>
2026-08-14 17:58:28 +0200

    Replace API authentication headers with Authorization

--- hassio_cloudflareipupdater/hassio_cloudflareipupdater/cloudflare.sh
+++ hassio_cloudflareipupdater/hassio_cloudflareipupdater/cloudflare.sh
@@ -77,8 +77,7 @@ echo "IP: $ip"
 
 # Fetches the zone information for the account
 zone_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE" \
-        -H "X-Auth-Email: $EMAIL" \
-        -H "X-Auth-Key: $API" \
+        -H "Authorization: $API" \
         -H "Content-Type: application/json")
 
 
@@ -103,8 +102,7 @@ echo "Zone $ZONE id : $zone_id"
 
 # Tries to fetch the record of the host
 dns_record_response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?name=$HOST" \
-        -H "X-Auth-Email: $EMAIL" \
-        -H "X-Auth-Key: $API" \
+        -H "Authorization: $API" \
         -H "Content-Type: application/json")
 
 if [[ $(jq <<<"$dns_record_response" -r '.success') != "true" ]]; then
@@ -147,8 +145,7 @@ if [[ -z $dns_record_id ]]; then
     echo "Creating new record for host $HOST"
 
     dns_record_response=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records" \
-            -H "X-Auth-Email: $EMAIL" \
-            -H "X-Auth-Key: $API" \
+            -H "Authorization: $API" \
             -H "Content-Type: application/json" \
             --data "$new_dns_record")
 else
@@ -157,8 +154,7 @@ else
     echo "Updating record $dns_record_id for host $HOST"
 
     dns_record_response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
-            -H "X-Auth-Email: $EMAIL" \
-            -H "X-Auth-Key: $API" \
+            -H "Authorization: $API" \
             -H "Content-Type: application/json" \
             --data "$new_dns_record")
 fi
